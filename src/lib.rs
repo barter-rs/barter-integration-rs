a1ab53dfecf26e6cf2c1dcdca1637e73cfe304c9
JustAStream <keirinvestment@gmail.com>
2022-03-26 12:02:16 +0000

    feat: added convenience impls for Instrument, InstrumentKind, Symbol

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,70 @@
-pub mod socket;
+use std::fmt::{Display, Formatter};
+use serde::{Deserialize, Deserializer, Serialize};
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+pub mod socket;
+pub mod public;
+
+#[derive(Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug, Deserialize, Serialize)]
+pub struct Instrument {
+    pub kind: InstrumentKind,
+    pub base: Symbol,
+    pub quote: Symbol,
+}
+
+impl Instrument {
+    pub fn new<S>(base: S, quote: S, kind: InstrumentKind) -> Self
+    where
+        S: Into<String>
+    {
+        Self {
+            kind,
+            base: Symbol::new(base),
+            quote: Symbol::new(quote),
+        }
+    }
+}
+
+#[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug, Deserialize, Serialize)]
+pub enum InstrumentKind {
+    Spot,
+    Future,
+}
+
+impl Default for InstrumentKind {
+    fn default() -> Self {
+        Self::Spot
+    }
+}
+
+impl Display for InstrumentKind {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+#[derive(Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug, Serialize)]
+pub struct Symbol(pub String);
+
+impl Display for Symbol {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+impl AsRef<str> for Symbol {
+    fn as_ref(&self) -> &str {
+        &self.0
+    }
+}
+
+impl<'de> Deserialize<'de> for Symbol {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error> where D: Deserializer<'de> {
+        String::deserialize(deserializer).map(Symbol::new)
+    }
+}
+
+impl Symbol {
+    pub fn new<S>(symbol: S) -> Self where S: Into<String> {
+        Self(symbol.into().to_lowercase())
     }
 }
