792963a058100f8572e369e9785c700f08e9a0ef
JustAStream <just-a-stream@users.noreply.github.com>
2022-11-05 12:54:52 +0000

    chore: rust docs

--- src/protocol/mod.rs
+++ src/protocol/mod.rs
@@ -5,15 +5,17 @@ use serde::de::DeserializeOwned;
 /// [`ProtocolParser`].
 pub mod websocket;
 
+/// Contains a private and public data HTTP client, and an associated exchange oriented HTTP
+/// request.
+pub mod http;
+
 /// `ProtocolParser`s are capable of parsing the input messages from a given protocol (eg WebSocket,
 /// Financial Information eXchange, etc) and deserialising into an `Output`.
 pub trait ProtocolParser {
     type Message;
     type Error;
 
-    fn parse<Output>(
-        input: Result<Self::Message, Self::Error>,
-    ) -> Option<Result<Output, SocketError>>
+    fn parse<Output>(input: Result<Self::Message, Self::Error>) -> Option<Result<Output, SocketError>>
     where
         Output: DeserializeOwned;
 }
