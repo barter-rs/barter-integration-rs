001ef3685569dc5f2c02dabb18164f4916429848
JustAStream <just-a-stream@users.noreply.github.com>
2022-12-05 12:03:06 +0000

    chore: transfer de helpers from b-data

--- src/de.rs
+++ src/de.rs
@@ -1,32 +1,54 @@
-use chrono::{DateTime, Utc};
-use serde::{de, ser::SerializeSeq, Serialize};
-use std::{str::FromStr, time::Duration};
 
 /// Determine the `DateTime<Utc>` from the provided `Duration` since the epoch.
-pub fn datetime_utc_from_epoch_duration(duration: Duration) -> DateTime<Utc> {
-    DateTime::<Utc>::from(std::time::UNIX_EPOCH + duration)
+pub fn datetime_utc_from_epoch_duration(
+    duration: std::time::Duration,
+) -> chrono::DateTime<chrono::Utc> {
+    chrono::DateTime::<chrono::Utc>::from(std::time::UNIX_EPOCH + duration)
 }
 
 /// Deserialize a `String` as the desired type.
 pub fn de_str<'de, D, T>(deserializer: D) -> Result<T, D::Error>
 where
-    D: de::Deserializer<'de>,
-    T: FromStr,
+    D: serde::de::Deserializer<'de>,
+    T: std::str::FromStr,
     T::Err: std::fmt::Display,
 {
-    let data: String = de::Deserialize::deserialize(deserializer)?;
-    data.parse::<T>().map_err(de::Error::custom)
+    let data: String = serde::de::Deserialize::deserialize(deserializer)?;
+    data.parse::<T>().map_err(serde::de::Error::custom)
 }
 
 /// Deserialize a `u64` as `DateTime<Utc>`.
-pub fn de_u64_epoch_ms_as_datetime_utc<'de, D>(deserializer: D) -> Result<DateTime<Utc>, D::Error>
+pub fn de_u64_epoch_ms_as_datetime_utc<'de, D>(
+    deserializer: D,
+) -> Result<chrono::DateTime<chrono::Utc>, D::Error>
 where
-    D: de::Deserializer<'de>,
+    D: serde::de::Deserializer<'de>,
 {
-    let epoch_ms: u64 = de::Deserialize::deserialize(deserializer)?;
-    Ok(datetime_utc_from_epoch_duration(Duration::from_millis(
-        epoch_ms,
-    )))
+    serde::de::Deserialize::deserialize(deserializer)
+        .map(|epoch_ms| datetime_utc_from_epoch_duration(std::time::Duration::from_millis(epoch_ms)))
+}
+
+/// Deserialize a &str "u64" as `DateTime<Utc>`.
+pub fn de_str_u64_epoch_ms_as_datetime_utc<'de, D>(
+    deserializer: D,
+) -> Result<chrono::DateTime<chrono::Utc>, D::Error>
+where
+    D: serde::de::Deserializer<'de>,
+{
+    de_str(deserializer)
+        .map(|epoch_ms| datetime_utc_from_epoch_duration(std::time::Duration::from_millis(epoch_ms)))
+}
+
+/// Deserialize a &str "f64" as `DateTime<Utc>`.
+pub fn de_str_f64_epoch_ms_as_datetime_utc<'de, D>(
+    deserializer: D,
+) -> Result<chrono::DateTime<chrono::Utc>, D::Error>
+where
+    D: serde::de::Deserializer<'de>,
+{
+    de_str(deserializer).map(|epoch_ms: f64| {
+        datetime_utc_from_epoch_duration(std::time::Duration::from_millis(epoch_ms as u64))
+    })
 }
 
 /// Assists deserialisation of sequences by attempting to extract & parse the next element in the
@@ -41,20 +63,22 @@ pub fn extract_next<'de, SeqAccessor, Target>(
     name: &'static str,
 ) -> Result<Target, SeqAccessor::Error>
 where
-    SeqAccessor: de::SeqAccess<'de>,
-    Target: de::DeserializeOwned,
+    SeqAccessor: serde::de::SeqAccess<'de>,
+    Target: serde::de::DeserializeOwned,
 {
     sequence
         .next_element::<Target>()?
-        .ok_or_else(|| de::Error::missing_field(name))
+        .ok_or_else(|| serde::de::Error::missing_field(name))
 }
 
 /// Serialize a generic element T as a `Vec<T>`.
 pub fn se_element_to_vector<T, S>(element: T, serializer: S) -> Result<S::Ok, S::Error>
 where
     S: serde::Serializer,
-    T: Serialize,
+    T: serde::Serialize,
 {
+    use serde::ser::SerializeSeq;
+
     let mut sequence = serializer.serialize_seq(Some(1))?;
     sequence.serialize_element(&element)?;
     sequence.end()
