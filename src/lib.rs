dc5adc74b99445207c539e9f21a39c72b8c757d8
JustAStream <just-a-stream@users.noreply.github.com>
2022-12-14 23:17:56 +0000

    chore: imports

--- src/lib.rs
+++ src/lib.rs
@@ -3,12 +3,10 @@
     missing_copy_implementations,
     rust_2018_idioms
 )]
-
-use crate::error::SocketError;
-use futures::{Sink, Stream};
-use pin_project::pin_project;
-use protocol::StreamParser;
-use serde::de::DeserializeOwned;
+use crate::{
+    protocol::StreamParser,
+    error::SocketError
+};
 use std::{
     collections::VecDeque,
     fmt::Debug,
@@ -16,6 +14,9 @@ use std::{
     pin::Pin,
     task::{Context, Poll},
 };
+use futures::Stream;
+use pin_project::pin_project;
+use serde::Deserialize;
 
 ///! # Barter-Integration
 ///! Contains an [`ExchangeStream`] capable of acting as a [`Stream`] for a given remote server, and a [`ExchangeSink`]
@@ -52,7 +53,7 @@ pub trait Validator {
 /// [`Transformer`]s are capable of transforming any `Input` into an iterator of
 /// `Result<Output, SocketError>`s.
 pub trait Transformer {
-    type Input: DeserializeOwned;
+    type Input: for<'de> Deserialize<'de>;
     type Output;
     type OutputIter: IntoIterator<Item = Result<Self::Output, SocketError>>;
     fn transform(&mut self, input: Self::Input) -> Self::OutputIter;
@@ -75,13 +76,12 @@ where
     pub protocol_marker: PhantomData<Protocol>,
 }
 
-impl<Protocol, InnerStream, StreamTransformer, ExchangeMessage> Stream
+impl<Protocol, InnerStream, StreamTransformer> Stream
     for ExchangeStream<Protocol, InnerStream, StreamTransformer>
 where
     Protocol: StreamParser,
     InnerStream: Stream<Item = Result<Protocol::Message, Protocol::Error>> + Unpin,
-    StreamTransformer: Transformer<Input = ExchangeMessage>,
-    ExchangeMessage: DeserializeOwned,
+    StreamTransformer: Transformer,
 {
     type Item = Result<StreamTransformer::Output, SocketError>;
 
@@ -100,7 +100,7 @@ where
             };
 
             // Parse input protocol message into `ExchangeMessage`
-            let exchange_message = match Protocol::parse::<ExchangeMessage>(input) {
+            let exchange_message = match Protocol::parse::<StreamTransformer::Input>(input) {
                 // `StreamParser` successfully deserialised `ExchangeMessage`
                 Some(Ok(exchange_message)) => exchange_message,
 
@@ -139,78 +139,78 @@ where
     }
 }
 
-/// Todo:
-#[derive(Debug)]
-#[pin_project]
-pub struct ExchangeSink<Protocol, InnerSink, SinkTransformer, Output>
-where
-    Protocol: StreamParser,
-    // Todo: may not be Protocol::Message
-    InnerSink: Sink<Protocol::Message>,
-    // Todo: Transformer may need to be double generic or have a Transformer and a Sink/StreamTransformer that's associated
-    SinkTransformer: Transformer,
-    Output: Debug,
-{
-    #[pin]
-    pub sink: InnerSink,
-    pub transformer: SinkTransformer,
-    pub buffer: VecDeque<Result<Output, SocketError>>,
-    pub protocol_marker: PhantomData<Protocol>,
-}
-
-impl<Protocol, InnerSink, SinkTransformer, Output> Sink<Protocol::Message>
-    for ExchangeSink<Protocol, InnerSink, SinkTransformer, Output>
-where
-    Protocol: StreamParser,
-    InnerSink: Sink<Protocol::Message>,
-    SinkTransformer: Transformer,
-    Output: Debug,
-{
-    type Error = SocketError;
-
-    fn poll_ready(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
-        self.project()
-            .sink
-            .poll_ready(cx)
-            .map_err(|_| SocketError::Sink)
-    }
-
-    fn start_send(self: Pin<&mut Self>, item: Protocol::Message) -> Result<(), Self::Error> {
-        self.project()
-            .sink
-            .start_send(item)
-            .map_err(|_| SocketError::Sink)
-    }
-
-    fn poll_flush(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
-        self.project()
-            .sink
-            .poll_flush(cx)
-            .map_err(|_| SocketError::Sink)
-    }
-
-    fn poll_close(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
-        self.project()
-            .sink
-            .poll_close(cx)
-            .map_err(|_| SocketError::Sink)
-    }
-}
-
-impl<Protocol, InnerSink, SinkTransformer, Output>
-    ExchangeSink<Protocol, InnerSink, SinkTransformer, Output>
-where
-    Protocol: StreamParser,
-    InnerSink: Sink<Protocol::Message>,
-    SinkTransformer: Transformer,
-    Output: Debug,
-{
-    pub fn new(sink: InnerSink, transformer: SinkTransformer) -> Self {
-        Self {
-            sink,
-            transformer,
-            buffer: VecDeque::with_capacity(6),
-            protocol_marker: PhantomData::default(),
-        }
-    }
-}
+// /// Todo:
+// #[derive(Debug)]
+// #[pin_project]
+// pub struct ExchangeSink<Protocol, InnerSink, SinkTransformer, Output>
+// where
+//     Protocol: StreamParser,
+//     // Todo: may not be Protocol::Message
+//     InnerSink: Sink<Protocol::Message>,
+//     // Todo: Transformer may need to be double generic or have a Transformer and a Sink/StreamTransformer that's associated
+//     SinkTransformer: Transformer<ExchangeMessage>,
+//     Output: Debug,
+// {
+//     #[pin]
+//     pub sink: InnerSink,
+//     pub transformer: SinkTransformer,
+//     pub buffer: VecDeque<Result<Output, SocketError>>,
+//     pub protocol_marker: PhantomData<Protocol>,
+// }
+//
+// impl<Protocol, InnerSink, SinkTransformer, Output> Sink<Protocol::Message>
+//     for ExchangeSink<Protocol, InnerSink, SinkTransformer, Output>
+// where
+//     Protocol: StreamParser,
+//     InnerSink: Sink<Protocol::Message>,
+//     SinkTransformer: Transformer<ExchangeMe>,
+//     Output: Debug,
+// {
+//     type Error = SocketError;
+//
+//     fn poll_ready(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+//         self.project()
+//             .sink
+//             .poll_ready(cx)
+//             .map_err(|_| SocketError::Sink)
+//     }
+//
+//     fn start_send(self: Pin<&mut Self>, item: Protocol::Message) -> Result<(), Self::Error> {
+//         self.project()
+//             .sink
+//             .start_send(item)
+//             .map_err(|_| SocketError::Sink)
+//     }
+//
+//     fn poll_flush(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+//         self.project()
+//             .sink
+//             .poll_flush(cx)
+//             .map_err(|_| SocketError::Sink)
+//     }
+//
+//     fn poll_close(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+//         self.project()
+//             .sink
+//             .poll_close(cx)
+//             .map_err(|_| SocketError::Sink)
+//     }
+// }
+//
+// impl<Protocol, InnerSink, SinkTransformer, Output>
+//     ExchangeSink<Protocol, InnerSink, SinkTransformer, Output>
+// where
+//     Protocol: StreamParser,
+//     InnerSink: Sink<Protocol::Message>,
+//     SinkTransformer: Transformer,
+//     Output: Debug,
+// {
+//     pub fn new(sink: InnerSink, transformer: SinkTransformer) -> Self {
+//         Self {
+//             sink,
+//             transformer,
+//             buffer: VecDeque::with_capacity(6),
+//             protocol_marker: PhantomData::default(),
+//         }
+//     }
+// }
