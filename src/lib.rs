23089c20dc525f28607f7422588d470e0ffec4ef
JustAStream <just-a-stream@users.noreply.github.com>
2023-01-08 15:54:40 +0000

    refactor: Transformer SocketError -> configurable assoc type

--- src/lib.rs
+++ src/lib.rs
@@ -48,11 +48,12 @@ pub trait Validator {
 }
 
 /// [`Transformer`]s are capable of transforming any `Input` into an iterator of
-/// `Result<Output, SocketError>`s.
+/// `Result<Self::Output, Self::Error>`s.
 pub trait Transformer {
+    type Error;
     type Input: for<'de> Deserialize<'de>;
     type Output;
-    type OutputIter: IntoIterator<Item = Result<Self::Output, SocketError>>;
+    type OutputIter: IntoIterator<Item = Result<Self::Output, Self::Error>>;
     fn transform(&mut self, input: Self::Input) -> Self::OutputIter;
 }
 
@@ -69,7 +70,7 @@ where
     #[pin]
     pub stream: InnerStream,
     pub transformer: StreamTransformer,
-    pub buffer: VecDeque<Result<StreamTransformer::Output, SocketError>>,
+    pub buffer: VecDeque<Result<StreamTransformer::Output, StreamTransformer::Error>>,
     pub protocol_marker: PhantomData<Protocol>,
 }
 
@@ -79,8 +80,9 @@ where
     Protocol: StreamParser,
     InnerStream: Stream<Item = Result<Protocol::Message, Protocol::Error>> + Unpin,
     StreamTransformer: Transformer,
+    StreamTransformer::Error: From<SocketError>,
 {
-    type Item = Result<StreamTransformer::Output, SocketError>;
+    type Item = Result<StreamTransformer::Output, StreamTransformer::Error>;
 
     fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
         loop {
@@ -102,7 +104,7 @@ where
                 Some(Ok(exchange_message)) => exchange_message,
 
                 // If `StreamParser` returns an Err pass it downstream
-                Some(Err(err)) => return Poll::Ready(Some(Err(err))),
+                Some(Err(err)) => return Poll::Ready(Some(Err(err.into()))),
 
                 // If `StreamParser` returns None it's a safe-to-skip message
                 None => continue,
@@ -114,7 +116,7 @@ where
                 .transform(exchange_message)
                 .into_iter()
                 .for_each(
-                    |output_result: Result<StreamTransformer::Output, SocketError>| {
+                    |output_result: Result<StreamTransformer::Output, StreamTransformer::Error>| {
                         self.buffer.push_back(output_result)
                     },
                 );
